41d1df1f1ac30fc5a475c255d33a6b1e6b9a82a5
lukasz.grebowski <lukasz.grebowski@asseco.pl>
2019-12-14 10:01:10 +0100

    Extend system draft protocol

--- protocols/system-draft/src/main/java/pl/wut/wsd/dsm/protocol/SystemDraftProtocol.java
+++ protocols/system-draft/src/main/java/pl/wut/wsd/dsm/protocol/SystemDraftProtocol.java
@@ -3,6 +3,7 @@ package pl.wut.wsd.dsm.protocol;
 import jade.lang.acl.ACLMessage;
 import lombok.Getter;
 import lombok.experimental.Accessors;
+import pl.wut.wsd.dsm.ontology.network.DraftSummary;
 import pl.wut.wsd.dsm.ontology.network.ExpectedInbalancement;
 import pl.wut.wsd.dsm.service.ServiceDescriptionFactory;
 
@@ -17,6 +18,9 @@ public class SystemDraftProtocol extends Protocol {
     private final ServiceDescriptionFactory serviceDescriptionFactory = new ServiceDescriptionFactory();
 
     @Getter
+    /**
+     * Informs quote manager of expected inbalancement. Quote manager then decides whether to start new draft.
+     */
     private TargetedStep<SystemDraftProtocol, ExpectedInbalancement> informQuoteManagerOfExpectedInbalancement =
             TargetedStep.<SystemDraftProtocol, ExpectedInbalancement>targetedBuilder()
                     .stepName("Inform quote manager of expected inbalancement")
@@ -27,5 +31,18 @@ public class SystemDraftProtocol extends Protocol {
                     .protocol(this)
                     .build();
 
+    /**
+     * After quote manager starts draft it should periodically update network advisory with draft details
+     * using this information network advisor can adjust demand profile.
+     */
+    private TargetedStep<SystemDraftProtocol, DraftSummary> updateWithDraftSummary =
+            TargetedStep.<SystemDraftProtocol, DraftSummary>targetedBuilder()
+                    .stepName("Update network advisory with current draft status")
+                    .performative(ACLMessage.INFORM)
+                    .required(true)
+                    .targetService(serviceDescriptionFactory.name("network-advisor"))
+                    .messageClass(DraftSummary.class)
+                    .protocol(this)
+                    .build();
 
 }
