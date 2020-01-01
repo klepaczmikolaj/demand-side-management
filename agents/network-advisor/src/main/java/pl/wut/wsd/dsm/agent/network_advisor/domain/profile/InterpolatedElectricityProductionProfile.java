fb505bf4e888fa436a06a411b6cc520ebd2d8444
lukasz.grebowski <lukasz.grebowski@asseco.pl>
2020-01-01 18:22:12 +0100

    Add legit interpolated electricity production profile and tests

--- agents/network-advisor/src/main/java/pl/wut/wsd/dsm/agent/network_advisor/domain/profile/InterpolatedElectricityProductionProfile.java
+++ agents/network-advisor/src/main/java/pl/wut/wsd/dsm/agent/network_advisor/domain/profile/InterpolatedElectricityProductionProfile.java
@@ -3,6 +3,7 @@ package pl.wut.wsd.dsm.agent.network_advisor.domain.profile;
 import lombok.RequiredArgsConstructor;
 
 import java.math.BigInteger;
+import java.time.Duration;
 import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.ListIterator;
@@ -22,14 +23,26 @@ public class InterpolatedElectricityProductionProfile implements ElectricityProd
 
         final Dates closestDates = findClosesDates(at, sortedDates);
 
-        return null;
+        final BigInteger y1 = productionPoints.get(closestDates.closestBefore);
+        final BigInteger y2 = productionPoints.get(closestDates.closestAfter);
+
+        final double secondDistance = closestDates.closestAfter.getSecond() - closestDates.closestBefore.getSecond();
+        final double x1Distance = Duration.between(closestDates.closestBefore, at).getSeconds();
+        final double x2Distance = Duration.between(closestDates.closestAfter, at).getSeconds();
+
+        return BigInteger.valueOf((long) ((x1Distance * y1.doubleValue() + x2Distance * y2.doubleValue()) / secondDistance));
     }
 
     private Dates findClosesDates(final ZonedDateTime at, final List<ZonedDateTime> sortedDates) {
         final ListIterator<ZonedDateTime> iterator = sortedDates.listIterator();
+        final ZonedDateTime last = sortedDates.get(sortedDates.size() - 1);
+        final ZonedDateTime beforeLast = sortedDates.get(sortedDates.size() - 2);
+        if (beforeLast.isBefore(at) && last.isBefore(at)) {
+            return Dates.of(beforeLast, last);
+        }
 
-        final ZonedDateTime next = iterator.next();
         while (iterator.hasNext()) {
+            final ZonedDateTime next = iterator.next();
             if (next.isAfter(at)) {
                 if (iterator.hasPrevious()) {
                     return Dates.of(iterator.previous(), next);
@@ -38,7 +51,7 @@ public class InterpolatedElectricityProductionProfile implements ElectricityProd
                 }
             }
         }
-        return null; //TODO, ostatnia i przedostatnia
+        throw new RuntimeException("xd");
     }
 
     @RequiredArgsConstructor(staticName = "of")
---
+++ agents/network-advisor/src/test/groovy/pl/wut/wsd/dsm/agent/network_advisor/domain/InterpolatedElectricityProductionProfileTest.groovy
@@ -0,0 +1,31 @@
+package pl.wut.wsd.dsm.agent.network_advisor.domain
+
+import pl.wut.wsd.dsm.agent.network_advisor.domain.profile.InterpolatedElectricityProductionProfile
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import java.time.ZonedDateTime
+
+class InterpolatedElectricityProductionProfileTest extends Specification {
+
+    private static final ZonedDateTime fixedTime = ZonedDateTime.now()
+
+    @Unroll
+    def 'Should provide nice result profile'() {
+        given:
+        final InterpolatedElectricityProductionProfile profile = new InterpolatedElectricityProductionProfile(productionPoints)
+
+        when:
+        profile.getProductionInWatts(fixedTime.plusHours(1))
+
+        then:
+        new BigInteger(expectedResult) == new BigInteger(expectedResult)
+
+        where:
+        productionPoints                                                                                | expectedResult
+        [(fixedTime): new BigInteger(0), (fixedTime.plusHours(2)): new BigInteger(100)]                 | 50
+        [(fixedTime): new BigInteger(100), (fixedTime.minusHours(1)): new BigInteger(50)]               | 150
+        [(fixedTime)              : new BigInteger(100), (fixedTime.minusHours(1)): new BigInteger(50),
+         (fixedTime.minusHours(3)): new BigInteger(200), (fixedTime.plusHours(5)): new BigInteger(500)] | 150
+    }
+}
