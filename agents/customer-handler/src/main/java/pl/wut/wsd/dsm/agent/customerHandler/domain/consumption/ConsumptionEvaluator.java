7a60c27464c0ce71b42eb9ae391d6089dbe09079
lukasz.grebowski <lukasz.grebowski@asseco.pl>
2020-01-29 13:14:31 +0100

    Null pointer fix

--- agents/customer-handler/src/main/java/pl/wut/wsd/dsm/agent/customerHandler/domain/consumption/ConsumptionEvaluator.java
+++ agents/customer-handler/src/main/java/pl/wut/wsd/dsm/agent/customerHandler/domain/consumption/ConsumptionEvaluator.java
@@ -1,6 +1,7 @@
 package pl.wut.wsd.dsm.agent.customerHandler.domain.consumption;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import pl.wut.wsd.dsm.agent.customerHandler.domain.model.Customer;
 import pl.wut.wsd.dsm.agent.customerHandler.domain.model.Obligation;
 import pl.wut.wsd.dsm.protocol.consumption.EnergyConsumption;
@@ -9,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@Slf4j
 @RequiredArgsConstructor
 public class ConsumptionEvaluator {
 
@@ -17,7 +19,10 @@ public class ConsumptionEvaluator {
     }
 
     private double calculateTimeNotKept(final Customer customer, final Obligation obligation, final List<EnergyConsumption> consumptions) {
-
+        if (consumptions == null) {
+            log.error("Consumptions for obligation {} are null, returning valid, please fix" + obligation.getId());
+            return 100;
+        }
         final Map<Boolean, List<EnergyConsumption>> isKept = consumptions.stream()
                 .collect(Collectors.partitioningBy(c ->
                         obligation.getRelatedOffer().isReduction() ? isReductionKept(c.getWattsConsumption(), obligation, customer) :
